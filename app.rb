b0d41fe16c2f3059837fbd3b9f473b5937463885
Sean Hsieh <seanyen0507@gmail.com>
2014-11-09 23:11:53 +0800

    add post function

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require 'sinatra/namespace'
 require 'NBA_info'
 require 'json'
 
 # Simple version of nba_scrapper
 class NBACatcherApp < Sinatra::Base
+  register Sinatra::Namespace
+
+  configure :production, :development do
+    enable :logging
+  end
+
   helpers do
     @profile_after = {}
     def get_profile(playername)
@@ -23,32 +30,67 @@ class NBACatcherApp < Sinatra::Base
       end
     end
 
-    def get_start_lineup(playername)
-
+    def check_start_lineup(playernames)
+      @lineup = {}
       sean = Scraper.new
-      is_null = 'true'
-      coming_game = Hash[sean.game[0].zip(sean.game[2])]
-      coming_game.each do |key, value|
-        if value.include?(playername)
-          @profile_after['game'].push(key)
-          @profile_after['competitor'].push(value)
-          is_null = 'false'
+      begin
+        playernames.each do |playername|
+          sean.game[2].each do |playerlist|
+            if playerlist.include?(playername)
+              @lineup[playername] = 'Yes, he is start line up today'
+            end
+          end
         end
+      rescue
+        halt 404
+      else
+        @lineup
       end
-      if is_null
-        @profile_after['game'].push('status' => 'noGame')
-        @profile_after['competitor'].push('competitors' => 'noGame')
-      end
-      @profile_after
     end
   end
-  get '/api/v1/player/:playername.json' do
-    content_type :json
-    get_profile(params[:playername]).to_json
+      # sean = Scraper.new
+      # is_null = 'true'
+      # coming_game = Hash[sean.game[0].zip(sean.game[2])]
+      # coming_game.each do |key, value|
+      #   if value.include?(playername)
+      #     @profile_after['game'].push(key)
+      #     @profile_after['competitor'].push(value)
+      #     is_null = 'false'
+      #   end
+      # end
+      # if is_null
+      #   @profile_after['game'].push('status' => 'noGame')
+      #   @profile_after['competitor'].push('competitors' => 'noGame')
+      # end
+      # @profile_after
+
+  get '/' do
+    'Simmple NBA catcher api/v1 is up and working'
   end
-  get '/api/v1/game/:playername.json' do
-    content_type :json
-    get_profile(params[:playername]).to_json
-    get_start_lineup(params[:playername]).to_json
+
+  namespace '/api/v1' do
+    get '/player/:playername.json' do
+      content_type :json
+      get_profile(params[:playername]).to_json
+    end
+    post '/check' do
+      content_type :json
+      begin
+        req = JSON.parse(request.body.read)
+        logger.info req
+      rescue
+        halt 400
+      end
+
+      playernames = req['playernames']
+      # startlineup = req['startlineup']
+      check_start_lineup(playernames).to_json
+    end
+
+    # get '/game/:playername.json' do
+    #   content_type :json
+    #   get_profile(params[:playername]).to_json
+    #   get_start_lineup(params[:playername]).to_json
+    # end
   end
 end
