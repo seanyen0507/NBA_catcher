f80f4f93ee7cb7edda62b9f7ef431bea104b83b0
hsuchinwang <sam30278@gmail.com>
2014-11-13 11:31:27 +0800

    Modufy some testing truble

--- Gemfile
+++ Gemfile
@@ -4,3 +4,10 @@ gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'NBA_info'
 gem 'json'
+
+group :test do
+  gem 'minitest'
+  gem 'rack'
+  gem 'rack-test'
+  gem 'rake'
+end
--- app.rb
+++ app.rb
@@ -12,21 +12,20 @@ class NBACatcherApp < Sinatra::Base
   end
 
   helpers do
-    @profile_after = {}
     def get_profile(playername)
       sam = Scraper.new
-      @profile_after = {
-        'name' => playername, 'profiles' => [], 'game' => [], 'competitor' => []
+      profile_after = {
+        'name' => playername, 'profiles' => []
       }
       begin
         name = params[:playername]
         sam.profile(name)[0].each do |key, value|
-          @profile_after['profiles'].push('Box-score' => key, 'Record' => value)
+          profile_after['profiles'].push('Box-score' => key, 'Record' => value)
         end
       rescue
         halt 404
       else
-        @profile_after
+        profile_after
       end
     end
 
